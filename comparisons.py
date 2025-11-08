7914f4c8881a49bc9541ebfe7432a98a9524d1a9
William H. Grover <wgrover@gmail.com>
2025-11-08 09:57:36 -0800

    Update comparisons.py

--- comparisons.py
+++ comparisons.py
@@ -1,16 +1,26 @@
 # /// script
 # requires-python = ">=3.14"
-# dependencies = []
+# dependencies = ["matplotlib", "numpy"]
 # ///
 
 import json
 import sys
+import matplotlib.pyplot as plt
+import numpy
+from matplotlib.colors import LogNorm
 
 f = open(sys.argv[1], 'r')
 d = json.load(f)
+bigtable = numpy.zeros((len(d),len(d)))
 
-for l in d:
+for x, l in enumerate(d):
 	print()
 	print(l)
-	for i in d[l]:
+	for y, i in enumerate(d[l]):
 		print(i[1], i[3], i[4])
+		bigtable[x][y] = i[4]
+
+
+fig, ax = plt.subplots()
+im = ax.imshow(bigtable, norm=LogNorm(vmin=1, vmax=100000))
+plt.show()
