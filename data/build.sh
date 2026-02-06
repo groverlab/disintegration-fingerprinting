a77dc4c518c2bf90ba85e8b5a5037b49d1607992
William H. Grover <wgrover@gmail.com>
2026-02-05 19:12:06 -0800

    build all figures

--- data/build.sh
+++ data/build.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 echo "Building all figures..."
 
-# uv run ../df-analyze.py beads --figure beads
-# uv run ../df-analyze.py aspirin --figure aspirin
-# uv run ../df-analyze.py variety --figure variety
-# uv run ../df-analyze.py long --figure long
+uv run ../df-analyze.py beads --figure beads
+uv run ../df-analyze.py aspirin --figure aspirin
+uv run ../df-analyze.py variety --figure variety
+uv run ../df-analyze.py long --figure long
 
-# uv run ../df-analyze.py bayer_tylenol/ --figure bayer_tylenol
+uv run ../df-analyze.py bayer_tylenol/ --figure bayer_tylenol
 
-# uv run ../df-analyze.py bayer_tylenol_only/ --figure bayer_tylenol_only
-# uv run ../df-analyze.py bayer_tylenol_only_subdirs/ --figure bayer_tylenol_only_subdirs
+uv run ../df-analyze.py bayer_tylenol_only/ --figure bayer_tylenol_only
+uv run ../df-analyze.py bayer_tylenol_only_subdirs/ --figure bayer_tylenol_only_subdirs
 
 uv run ../df-heatmap.py aspirin\ comparisons\ self.json --figure aspirin
 uv run ../df-heatmap.py bayer_tylenol_only\ comparisons\ self.json --figure bayer_tylenol_only
